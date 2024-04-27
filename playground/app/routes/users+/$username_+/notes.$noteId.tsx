4c292591cecc01418415ec788304bd7bf3b5315d
Andre Roussakoff <madrus@gmail.com>
2024-04-27 17:51:37 +0200

    before

--- playground/app/routes/users+/$username_+/notes.$noteId.tsx
+++ playground/app/routes/users+/$username_+/notes.$noteId.tsx
@@ -2,11 +2,9 @@ import { useParams } from '@remix-run/react'
 
 export default function NoteRoute() {
 	const params = useParams()
-
 	return (
 		<div className="container pt-12 border-8 border-red-500">
-			{/* 🐨 render the note id from useParams */}
-			<h2 className="text-h2">{params.noteId?.split('-').join(' ')}</h2>
+			<h2 className="text-h2">{params.noteId}</h2>
 		</div>
 	)
 }
