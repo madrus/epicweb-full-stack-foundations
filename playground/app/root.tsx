ffc290855d93670b8664053554330e677d493d52
Andre Roussakoff <madrus@gmail.com>
2024-05-08 07:52:20 +0200

    after

--- playground/app/root.tsx
+++ playground/app/root.tsx
@@ -1,4 +1,5 @@
 import os from 'node:os'
+
 import { cssBundleHref } from '@remix-run/css-bundle'
 import { json, type LinksFunction } from '@remix-run/node'
 import {
@@ -6,12 +7,13 @@ import {
 	Links,
 	LiveReload,
 	Meta,
+	type MetaFunction,
 	Outlet,
 	Scripts,
 	ScrollRestoration,
 	useLoaderData,
-	type MetaFunction,
 } from '@remix-run/react'
+
 import faviconAssetUrl from './assets/favicon.svg'
 import { GeneralErrorBoundary } from './components/error-boundary.tsx'
 import { EpicShop } from './epicshop.tsx'
@@ -36,12 +38,7 @@ export async function loader() {
 // 🐨 Create a Document component here that renders almost everything that's in
 // the App with the exception of the visual stuff in the body. It should not
 // use useLoaderData because we can't rely on that in the error case.
-
-export default function App() {
-	// throw new Error('🐨 root component error')
-	const data = useLoaderData<typeof loader>()
-	// 🐨 replace most of this with the <Document> component and render the
-	// header, outlet, and footer inside of it.
+function Document({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" className="h-full overflow-x-hidden">
 			<head>
@@ -51,41 +48,53 @@ export default function App() {
 				<Links />
 			</head>
 			<body className="flex h-full flex-col justify-between bg-background text-foreground">
-				<header className="container mx-auto py-6">
-					<nav className="flex justify-between">
-						<Link to="/">
-							<div className="font-light">epic</div>
-							<div className="font-bold">notes</div>
-						</Link>
-						<Link className="underline" to="users/kody/notes/d27a197e">
-							Kody's Notes
-						</Link>
-					</nav>
-				</header>
-
-				<div className="flex-1">
-					<Outlet />
-				</div>
-
-				<div className="container mx-auto flex justify-between">
-					<Link to="/">
-						<div className="font-light">epic</div>
-						<div className="font-bold">notes</div>
-					</Link>
-					<p>Built with ♥️ by {data.username}</p>
-				</div>
-				<div className="h-5" />
+				{children}
 				<ScrollRestoration />
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `window.ENV = ${JSON.stringify(data.ENV)}`,
-					}}
-				/>
 				<Scripts />
 				<EpicShop />
 				<LiveReload />
 			</body>
 		</html>
+	)
+}
+
+export default function App() {
+	// throw new Error('🐨 root component error')
+	const data = useLoaderData<typeof loader>()
+	// 🐨 replace most of this with the <Document> component and render the
+	// header, outlet, and footer inside of it.
+	return (
+		<Document>
+			<header className="container mx-auto py-6">
+				<nav className="flex justify-between">
+					<Link to="/">
+						<div className="font-light">epic</div>
+						<div className="font-bold">notes</div>
+					</Link>
+					<Link className="underline" to="users/kody">
+						Kody
+					</Link>
+				</nav>
+			</header>
+
+			<div className="flex-1">
+				<Outlet />
+			</div>
+
+			<div className="container mx-auto flex justify-between">
+				<Link to="/">
+					<div className="font-light">epic</div>
+					<div className="font-bold">notes</div>
+				</Link>
+				<p>Built with ♥️ by {data.username}</p>
+			</div>
+			<div className="h-5" />
+			<script
+				dangerouslySetInnerHTML={{
+					__html: `window.ENV = ${JSON.stringify(data.ENV)}`,
+				}}
+			/>
+		</Document>
 	)
 }
 
@@ -98,5 +107,12 @@ export const meta: MetaFunction = () => {
 
 export function ErrorBoundary() {
 	// 🐨 render the GeneralErrorBoundary in your new Document component.
-	return <GeneralErrorBoundary />
+	// wrap it in the style like that of the outlet hereabove
+	return (
+		<Document>
+			<div className="flex-1">
+				<GeneralErrorBoundary />
+			</div>
+		</Document>
+	)
 }
